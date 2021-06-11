205e9f274922b23d83386ada9d1bb91adf41b232
Amy Pellegrini <amyp@motabilityoperations.co.uk>
2021-06-11 16:56:14 +0100

    Provide complexity analysis for swap algorithm

--- src/swap.js
+++ src/swap.js
@@ -1,3 +1,13 @@
+/**
+ * In-place array swap.
+ *
+ * Time complexity: O(1)
+ * Space complexity: O(1)
+ *
+ * @param {number[]} array
+ * @param {number} idx1
+ * @param {number} idx2
+ */
 function swap(array, idx1, idx2) {
   const tmp = array[idx1];
   array[idx1] = array[idx2];
